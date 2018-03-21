3b8ac72c71c0ea8730f754796ea1a0313384b715
apzz0928 <apzz0928@gmail.com>
2018-03-21 18:35:16 +0900

    2018/03/21 마케팅 유입 등록 관련 유효성체크 작업중

--- src/test/java/aceCounterPlus/signUp.java
+++ src/test/java/aceCounterPlus/signUp.java
@@ -14,6 +14,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 
 import static com.codeborne.selenide.Selenide.*;
@@ -26,7 +27,7 @@ public class signUp {
 	@SuppressWarnings("unused")
 	private static WebDriver driver;
 	@SuppressWarnings("unused")
-	private static String baseUrl, hubUrl, TestBrowser, id, pw, number, domain;
+	private static String baseUrl, hubUrl, TestBrowser, id, pw, pw1, number, domain;
 
 	@Parameters("browser")
 	@BeforeClass
@@ -35,7 +36,8 @@ public class signUp {
 		hubUrl = "http://10.77.129.79:5555/wd/hub";
 		id = "nhnace";
 		pw = "qordlf!@34";
-		number = "0003";
+		pw1 = "qordlf12#$";
+		number = "0007";
 		domain = "apzz";
 
 		String urlToRemoteWD = hubUrl;
@@ -75,13 +77,43 @@ public class signUp {
 	}
 
 	private static void js(String javaScriptSource) {
-		executeJavaScript(javaScriptSource);
+	    executeJavaScript(javaScriptSource);
 	}
-
-	@Test(priority = 0)
-	public void login() throws InterruptedException {
+	public static void alertCheck(String check1, String check2, int i) throws InterruptedException {
+		String alertCheck = "";
+		if(check1.equals("입력")) {
+			alertCheck = $("p", i).text();
+			Thread.sleep(1000);
+			if(alertCheck.equals(check2 + " 입력해주세요.")) {
+				$(".btn-sm", i+1).click();
+				System.out.println(" *** " + check2 + " 미입력 체크 성공 *** ");
+			} else {
+				System.out.println(" *** " + check2 + " 미입력 체크 실패 *** ");
+				close();
+			}
+		} else if (check1.equals("유효성")) {
+			alertCheck = $("p", i).text();
+			Thread.sleep(1000);
+			if(alertCheck.equals("한글, 영문(소문자), 숫자, 특수문자(!%()+-_=:./~#), 띄어쓰기로 입력하세요.")) {
+				$(".btn-sm", i+1).click();
+				System.out.println(" *** " + check2 + " 유효성 체크 성공 *** ");
+			} else {
+				System.out.println(" *** " + check2 + " 유효성 체크 실패 *** ");
+				close();
+			}
+		}
+	}
+	
+	//@Test(priority = 0)
+	public void login() {
 		open(baseUrl);
-		$(".go_signup").click();
+		String signUp = $(".go_signup").text();
+		if(signUp.contentEquals("회원가입")) {
+			System.out.println(" 회원가입 버튼 확인 완료! ");
+			$(".go_signup").click();		
+		} else {
+			System.out.println(" 회원가입 버튼을 찾을 수 없습니다! ");
+		}
 		$("input", 1).click();
 		js("$('#using').click();"); // 체크박스 체크가 안먹어서 js로
 		js("$('#info').click();");
@@ -91,26 +123,158 @@ public class signUp {
 		$("#userpw").setValue(pw);
 		$("#repeatpw").setValue(pw);
 		$("#usernm").setValue(id + number);
-		$("#useremail").setValue("apzz0928@nhnent.com");
+		$("#useremail").setValue("apzz0928@nate.com");
 		js("$('#news').click();"); // 체크박스 체크가 안먹어서 js로
 		$("#mp2").setValue("9743");
 		$("#mp3").setValue("0928");
 		$("#stepOneCompleted").click();
-		System.out.println(" ** -- * SignUp Step1 Pass * -- ** ");
+		String stepOne = $(".request").text(); 
+		if(stepOne.equals("※ 서비스 선택이 어려우신가요? 딱 맞는 서비스를 추천해 드립니다.추천 받기")) {
+			System.out.println(" *** 회원가입 1단계 약관동의 및 회원정보 입력 성공 *** ");			
+		} else {
+			System.out.println(" *** 회원가입 1단계 실패 *** ");
+			close();
+		}
 		$(By.name("input-domain")).setValue(domain + number + ".com");
 		$(".ace-btn-add-domain").waitUntil(visible, 1000);
 		$(By.name("largeCategoryCd")).selectOptionByValue("22");
 		$(By.name("middleCategoryCd")).selectOptionByValue("188");
-		Thread.sleep(1000);
 		$(".ace-btn-add-domain").click(); // 페이지 이동 속도때문에 도메인 체크를 나중에
 		$("#stepTwoCompleted").click();
-		System.out.println(" ** -- * SignUp Step2 Pass * -- ** ");
+		String stepTwo = $("h3").text();
+		if(stepTwo.equals("회원가입(무료체험) 신청이 완료되었습니다.")) {
+			System.out.println(" *** 회원가입 2단계 서비스 정보 입력, 회원가입 3단계 서비스 신청 완료 *** ");			
+		} else {
+			System.out.println(" *** 회원가입 2단계 실패 *** ");
+			close();
+		}
 		$(".btn_join").click();
 		$(".dropdown-toggle", 3).click();
 		$(".btn-logout").click();
-		System.out.println(" ** -- * SignUp Step2 Pass * -- ** ");
 	}
-
+	
+	//@Test(priority = 1)
+	public void changePassword() {
+		open(baseUrl);
+		$("#uid").setValue(id + number);
+		$("#upw").setValue(pw);
+		$(".btn_login").click();
+		String loginCheck = $(".btn_logout").text();
+		$(".btn_logout").getValue();
+		if(loginCheck.equals("로그아웃")) {
+			System.out.println(" *** 로그인 성공 *** ");
+		} else {
+			System.out.println(" *** 로그아웃 버튼을 찾을 수 없습니다. 로그인 여부를 확인해주세요 *** ");
+			close();
+		}
+		$(".go_setting").click();
+		$(By.linkText("회원정보")).click();
+		$("#pwd").setValue(pw);
+		$("#btn-ok").click();
+		System.out.println(" *** 비밀번호 수정 페이지 접근 성공 *** ");
+		$("#prePwd").setValue(pw);
+		$("#changePwd").setValue(pw1);
+		$("#changePwdConfirm").setValue(pw1);
+		$("#modifyProc").click();
+		$("#btn-modal-alert-yes").click();
+		String mbn = $(".mbn").text();
+		if(mbn.equals("비밀번호 변경이 완료되었습니다.")) {
+			$("#okButton").click();
+			System.out.println(" *** 비밀번호 변경 성공 *** ");
+		} else {
+			System.out.println(" *** 비밀번호 변경 실패 *** ");
+			close();
+		}
+		$("#prePwd").setValue(pw1);
+		$("#changePwd").setValue(pw);
+		$("#changePwdConfirm").setValue(pw);
+		$("#modifyProc").click();
+		$("#btn-modal-alert-yes").click();
+		if(mbn.equals("비밀번호 변경이 완료되었습니다.")) {
+			$("#okButton").click();
+			System.out.println(" *** 비밀번호 원복 성공 *** ");
+		} else {
+			System.out.println(" *** 비밀번호 원복 실패 *** ");
+			close();
+		}
+		$(".dropdown-toggle", 3).click();
+		$(".btn-logout").click();
+	}
+	@Test(priority = 2)
+	public void addMarketing() throws InterruptedException {
+		open(baseUrl);
+		$("#uid").setValue(id + number);
+		$("#upw").setValue(pw);
+		$(".btn_login").click();
+		String loginCheck = $(".btn_logout").text();
+		$(".btn_logout").getValue();
+		if(loginCheck.equals("로그아웃")) {
+			System.out.println(" *** 로그인 성공 *** ");
+		} else {
+			System.out.println(" *** 로그아웃 버튼을 찾을 수 없습니다. 로그인 여부를 확인해주세요 *** ");
+			close();
+		}
+		$(".go_stat", 1).click();
+		String pageLoadingCheck = $(".ace-svc-name").text();
+		if(pageLoadingCheck.equals(domain + number + ".com")) {
+			System.out.println(" *** 통계보기 페이지 접근 성공 *** ");
+		} else {
+			System.out.println(" *** 통계보기 페이지 접근 실패 *** ");
+			close();
+		}
+		$("#redirectConfBtn").click();
+		pageLoadingCheck = $(".notokr-bold").text();
+		if(pageLoadingCheck.equals("마케팅 유입 설정")) {
+			System.out.println(" *** 마케팅 유입 설정 페이지 접근 성공 *** ");
+		} else {
+			System.out.println(" *** 마케팅 유입 설정 페이지 접근 실패 *** ");
+			close();
+		}
+		$("#inflowAddBtn").click();
+		js("$('#channelDcd2').click();");
+		$("#btnReg").click();
+		alertCheck("입력", "캠페인명을", 4);
+		$("#campaign_nm").setValue(domain + number + ".com@");
+		$("#btnReg").click();
+		alertCheck("유효성", "캠페인명", 5);
+		$("#campaign_nm").setValue(domain + number + ".com");
+		$("#btnReg").click();
+		alertCheck("입력", "소재를", 6);
+		$("#campaign_material_value0").setValue(domain + number + ".com@");
+		$("#btnReg").click();
+		alertCheck("유효성", "소재", 7);
+		$("#campaign_material_value0").setValue(domain + number + ".com");
+		$("#btnReg").click();
+		//alertCheck("입력", "연결 URL을 ", 8); alert 문구가 하세요. 라서 공통화 못시킴 문구 통일 필요
+		String msgCheck = $("p", 8).text();
+		if(msgCheck.equals("연결 URL을 입력하세요.")) {
+			$(".btn-sm", 9).click();
+			System.out.println(" *** 연결 URL 미입력 체크 성공 *** ");
+		} else {
+			System.out.println(" *** 연결 URL 미입력 체크 실패 *** ");
+			close();
+		}
+		$("#original_url0").setValue("a");
+		$("#btnReg").click();
+		msgCheck = $("p", 9).text();
+		if(msgCheck.equals("올바른 URL을 입력하세요.")) {
+			$(".btn-sm", 10).click();
+			System.out.println(" *** 연결 URL 유효성 체크 성공 *** ");
+		} else {
+			System.out.println(" *** 연결 URL 유효성 체크 실패 *** ");
+			close();
+		}
+		$("#original_url0").setValue(domain + number + ".com");
+		$("#btnReg").click();
+		msgCheck = $("p", 10).text();
+		if(msgCheck.equals("등록에 성공했습니다.")) {
+			$(".btn-sm", 11).click();
+			System.out.println(" *** 마케팅 유입 설정 추가 성공 *** ");
+		} else {
+			System.out.println(" *** 마케팅 유입 설정 추가 실패 *** ");
+			close();
+		}
+	}
 	@AfterClass
 	public void afterTest() {
 		closeWebDriver();
